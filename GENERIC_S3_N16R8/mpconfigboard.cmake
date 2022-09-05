211f292dfb186e4afacf7a0e598818bd1174d847
Myth <lxbpxylps@126.com>
2022-09-06 00:26:26 +0800

    fix: spiram mode

--- GENERIC_S3_N16R8/mpconfigboard.cmake
+++ GENERIC_S3_N16R8/mpconfigboard.cmake
@@ -3,6 +3,6 @@ set(IDF_TARGET esp32s3)
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.usb
-    boards/sdkconfig.spiram_sx
+    boards/GENERIC_S3_N16R8/sdkconfig.spiram_sx
     boards/GENERIC_S3_N16R8/sdkconfig.board
 )
